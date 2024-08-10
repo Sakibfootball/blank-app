4a8b542cca2afc1523373c3417ef1d47fe529dcf
Sakib Haider <62951194+Sakibfootball@users.noreply.github.com>
2024-08-10 23:10:17 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,49 @@ st.markdown(
       justify-content: center; /* Center horizontally */
       align-items: center; /* Center vertically */
       cursor: pointer; /* Pointer cursor on hover */
-      transition: background-color 0.3s ease, transform 0.3s ease;
         }
         </style>""",
     unsafe_allow_html=True,
 )
 
 st.button("STOP")
+st.markdown(
+    """<style>
+        .element-container:nth-of-type(3) button {
+            width: 50vw; /* 50% of the viewport width */
+              height: 10vh; /* 50% of the viewport height */
+          background-color: #ADD8E6; /* Green background */
+      color: white; /* White text color */
+      font-size: 6rem; /* Font size */
+      border: none; /* Remove border */
+      border-radius: 10px; /* Rounded corners */
+      display: flex; /* Flexbox to center content */
+      justify-content: center; /* Center horizontally */
+      align-items: center; /* Center vertically */
+      cursor: pointer; /* Pointer cursor on hover */
+        }
+        </style>""",
+    unsafe_allow_html=True,
+)
 
 st.button("PLAY")
+st.markdown(
+    """<style>
+        .element-container:nth-of-type(3) button {
+            width: 50vw; /* 50% of the viewport width */
+              height: 10vh; /* 50% of the viewport height */
+          background-color: #ADD8E6; /* Green background */
+      color: white; /* White text color */
+      font-size: 6rem; /* Font size */
+      border: none; /* Remove border */
+      border-radius: 10px; /* Rounded corners */
+      display: flex; /* Flexbox to center content */
+      justify-content: center; /* Center horizontally */
+      align-items: center; /* Center vertically */
+      cursor: pointer; /* Pointer cursor on hover */
+        }
+        </style>""",
+    unsafe_allow_html=True,
+)
 
 st.button("PAUSE")
