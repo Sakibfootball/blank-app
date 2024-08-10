6b5420aa9b96045ce4110b5b3b15814f7e2a6d48
Sakib Haider <62951194+Sakibfootball@users.noreply.github.com>
2024-08-10 23:30:12 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,30 @@ import streamlit as st
 st.title("🎈 My new app")
 st.markdown(
     """
+    <style>
+    .custom-buttons button {
+        width: 50vw !important; 
+        height: 10vh !important;
+        background-color: #ADD8E6 !important; 
+        color: white !important; 
+        font-size: 4rem !important;
+        border: none !important; 
+        border-radius: 10px !important; 
+        cursor: pointer !important; 
+        transition: background-color 0.3s ease, transform 0.3s ease !important; 
+    }
+    
+    .custom-buttons button:hover {
+        background-color: #87CEFA !important; 
+        transform: scale(1.05) !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
     <div class="custom-buttons">
         <button id="button1">Button 1</button>
         <button id="button2">Button 2</button>
