f0619f8ec6ce60a6589042c46bf2023c04d48e13
Sakib Haider <62951194+Sakibfootball@users.noreply.github.com>
2024-08-10 23:14:53 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,60 +4,26 @@ st.title("🎈 My new app")
 st.markdown(
     """<style>
         .element-container:nth-of-type(3) button {
-            width: 50vw; /* 50% of the viewport width */
-              height: 10vh; /* 50% of the viewport height */
-          background-color: #ADD8E6; /* Green background */
-      color: white; /* White text color */
-      font-size: 6rem; /* Font size */
-      border: none; /* Remove border */
-      border-radius: 10px; /* Rounded corners */
-      display: flex; /* Flexbox to center content */
-      justify-content: center; /* Center horizontally */
-      align-items: center; /* Center vertically */
-      cursor: pointer; /* Pointer cursor on hover */
+          width: 50vw; 
+          height: 10vh; 
+          background-color: #ADD8E6;
+          color: white; 
+          font-size: 6rem;
+          border: none; 
+          border-radius: 10px; 
+          display: flex; 
+          justify-content: center;
+          align-items: center; 
+          cursor: pointer;
         }
         </style>""",
     unsafe_allow_html=True,
 )
 
 st.button("STOP")
-st.markdown(
-    """<style>
-        .element-container:nth-of-type(3) button {
-            width: 50vw; /* 50% of the viewport width */
-              height: 10vh; /* 50% of the viewport height */
-          background-color: #ADD8E6; /* Green background */
-      color: white; /* White text color */
-      font-size: 6rem; /* Font size */
-      border: none; /* Remove border */
-      border-radius: 10px; /* Rounded corners */
-      display: flex; /* Flexbox to center content */
-      justify-content: center; /* Center horizontally */
-      align-items: center; /* Center vertically */
-      cursor: pointer; /* Pointer cursor on hover */
-        }
-        </style>""",
-    unsafe_allow_html=True,
-)
+
 
 st.button("PLAY")
-st.markdown(
-    """<style>
-        .element-container:nth-of-type(3) button {
-            width: 50vw; /* 50% of the viewport width */
-              height: 10vh; /* 50% of the viewport height */
-          background-color: #ADD8E6; /* Green background */
-      color: white; /* White text color */
-      font-size: 6rem; /* Font size */
-      border: none; /* Remove border */
-      border-radius: 10px; /* Rounded corners */
-      display: flex; /* Flexbox to center content */
-      justify-content: center; /* Center horizontally */
-      align-items: center; /* Center vertically */
-      cursor: pointer; /* Pointer cursor on hover */
-        }
-        </style>""",
-    unsafe_allow_html=True,
-)
+
 
 st.button("PAUSE")
